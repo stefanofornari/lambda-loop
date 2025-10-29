dbdfc4d56fa8ddfead0d7fc1b79e65b99dcc4047
Stefano Fornari <stefano.fornari@gmail.com>
2025-10-30 00:03:29 +0100

    Initial revision - comparison between llup and forEach/stream

---
+++ src/test/java/ste/lloop/Comparison.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2025 the original author or authors from the λLoop project (https://lambda-loop.github.io/)..
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ste.lloop;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ */
+public class Comparison {
+
+    @Test
+    public void comparison() {
+        AtomicInteger ai = new AtomicInteger();
+        long start = System.currentTimeMillis();
+        for (int j=0; j<100000; ++j) {
+            Loop.on().from(1).to(10).loop((i) -> ai.incrementAndGet());
+        }
+        System.out.println(System.currentTimeMillis()-start);
+
+        ai.set(0);
+        start = System.currentTimeMillis();
+        for (int j=0; j<100000; ++j) {
+            List.of(1, 2, 3, 4, 5, 6, 7,8 ,9 ,10).forEach((i) -> ai.incrementAndGet());
+        }
+        System.out.println(System.currentTimeMillis()-start);
+    }
+}
